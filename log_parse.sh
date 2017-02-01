ca27ec3856a0290231c4c0680d2ed0c72d30f3cd
Joseph Stacy <mekanical@gmail.com>
2017-02-01 07:16:58 -0800

    added export comment

--- log_parse.sh
+++ log_parse.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+#export LISTENER_IP=X.X.X.X before run
 conntrack -d $LISTENER_IP -E -p tcp --dport 3389 | sed 's/^.*dst=//' >> ~/blacklist_buffer &
 echo $! > ~/log_3389.pid
 while true
  do
-  if [[ -f ~/blacklist_buffer ]]
+  if [[ -f blacklist_buffer ]]
    then
-    awk '{print $1}' ~/blacklist_buffer | uniq >> ~/blacklist
-    rm ~/blacklist_buffer
+    awk '{print $1}' ~/blacklist_buffer | uniq >> blacklist
+    rm blacklist_buffer
   fi
-  sort -u ~/blacklist
+  sort -u blacklist
   sleep 60
  done
